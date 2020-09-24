c0020429b6e14374726cc859c9d7a9d5a94e528c
Jnmendza <jonatmendoza@gmail.com>
2020-09-23 23:14:58 -0700

    grabbing token from url and auth with spotify up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Login from "./components/Login"
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
+  const [token, setToken] = useState(null)
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    // Securtiy purpose 
+    window.location.hash= "";
+    const _token = hash.access_token;
+
+    if(token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log(user)
+      })
+    }
+  }, []);
+
+
   return (
     <div className="app">
-      <Login />
+    {
+      token ? (
+        <h1>I am logged in</h1>
+      ) : (
+        <Login />
+      )
+    }
     </div>
   );
 }
